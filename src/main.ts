29ec760ea1f06f2b078dcbb68ec3ddd5376342ea
Brian Koey <briankoey@gmail.com>
2018-09-14 13:17:55 +0800

    add apollo-express

--- src/main.ts
+++ src/main.ts
@@ -1,49 +1,17 @@
-import { ApolloServer, gql } from 'apollo-server';
+import { ApolloServer, gql } from 'apollo-server-express';
+import express from 'express';
 
-const books = [
-  {
-    author: 'J.K. Rowling',
-    title: 'Harry Potter and the Chamber of Secrets',
-  },
-  {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton',
-  },
-];
+import schema from './schema';
 
-// Type definitions define the "shape" of your data and specify
-// which ways the data can be fetched from the GraphQL server.
-const typeDefs = gql`
-  # Comments in GraphQL are defined with the hash (#) symbol.
+const app = express();
 
-  # This "Book" type can be used in other type declarations.
-  type Book {
-    title: String
-    author: String
-  }
+const server = new ApolloServer({
+  schema,
+});
 
-  # The "Query" type is the root of all GraphQL queries.
-  # (A "Mutation" type will be covered later on.)
-  type Query {
-    books: [Book]
-  }
-`;
-
-// Resolvers define the technique for fetching the types in the
-// schema.  We'll retrieve books from the "books" array above.
-const resolvers = {
-  Query: {
-    books: () => books,
-  },
-};
-
-// In the most basic sense, the ApolloServer can be started
-// by passing type definitions (typeDefs) and the resolvers
-// responsible for fetching the data for those types.
-const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app });
 
 // This `listen` method launches a web-server.  Existing apps
 // can utilize middleware options, which we'll discuss later.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+app.listen({ port: 4000 }), () =>
+console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
